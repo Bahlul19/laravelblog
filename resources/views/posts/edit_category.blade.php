789297e80fc180dfe3adf50aac48277d4806eb88
Bahlul19 <bahlul.siddiquee@sjinnovation.com>
2020-02-07 18:18:05 +0600

    edit page added

---
+++ resources/views/posts/edit_category.blade.php
@@ -0,0 +1,48 @@
+@extends('welcome')
+@section('content')
+<div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <p>
+        <a href="{{ route('add.category') }}" class="btn btn-danger">Add Category</a>
+            <a href="{{ route('all.category') }}" class="btn btn-info">List Category</a>
+        </p>
+
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+      <form action="{{ route('update.category') }}">
+          @csrf
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>Category Name</label>
+            <input type="text" name="name" class="form-control" value="{{ $category->name }}" id="name"  data-validation-required-message="Please category name.">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+                <label>Category Slug</label>
+                <input type="text" name="slug" class="form-control" value="{{ $category->slug }}" id="slug"  data-validation-required-message="Please category slug.">
+              <p class="help-block text-danger"></p>
+            </div>
+        </div>
+
+          <br>
+          <div id="success"></div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary" id="sendMessageButton">Send</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
