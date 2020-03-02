ac1aaddf2b973e70bd86ad4c1462474801fbee6c
Bahlul <bahlul.siddiquee@sjinnovation.com>
2020-03-02 16:48:04 +0600

    Path change into the master file for localsetup into the home pc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
   <title>Clean Blog - Start Bootstrap Theme</title>
 
   <!-- Bootstrap core CSS -->
-<link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('public/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -19,7 +19,7 @@
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
   <!-- Custom styles for this template -->
-  <link href="{{ asset('assets/css/clean-blog.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('public/assets/css/clean-blog.min.css') }}" rel="stylesheet">
 
 </head>
 
@@ -120,7 +120,7 @@
 <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
   <!-- Custom scripts for this template -->
-<script src="{{ asset('assets/js/clean-blog.min.js') }}"></script>
+<script src="{{ asset('public/assets/js/clean-blog.min.js') }}"></script>
 
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
